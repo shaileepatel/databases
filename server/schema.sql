2234c377379b0b1276e75efdb67b88b46e6e4414
Shailee Patel <shaileeee@gmail.com>
2020-10-28 17:32:28 -0700

    create table users and messages

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,21 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20),
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  msg VARCHAR(50),
+  user_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
